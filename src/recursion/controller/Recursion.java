31ab3667a27387fd0bb284966b39246ac4109e19
FitForAKing47 <natethetate90@gmail.com>
2014-01-21 11:55:32 -0700

    fib
    fibrecursion project

---
+++ src/recursion/controller/Recursion.java
@@ -0,0 +1,11 @@
+package recursion.controller;
+
+public class Recursion
+{
+
+	public static void main(String [] args)
+	{
+		FibRecursion myFibRecursion = new FibRecursion();
+		myFibRecursion.start();
+	}
+}
